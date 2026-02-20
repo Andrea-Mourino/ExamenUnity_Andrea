b8b6d227f1c6ef23d892e2d0f5c774b30145e854
Andrea-Mourino <amourinodesousa@danielcastelao.org>
2026-02-20 10:47:02 +0100

    Add files via upload

---
+++ Assets/PlayerControler.cs
@@ -0,0 +1,24 @@
+using UnityEngine; 
+ 
+public class PlayerControler : MonoBehaviour { 
+    public float speed = 15f; 
+    private Rigidbody rb; 
+ 
+    void Start() { 
+        rb = GetComponent<Rigidbody>();
+    } 
+ 
+    void FixedUpdate() { 
+        float moveHorizontal = Input.GetAxis("Horizontal"); 
+        float moveVertical = Input.GetAxis("Vertical"); 
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical); 
+        rb.AddForce(movement * speed); //Aqui va la fuerza aplicada
+    } 
+ 
+    private void OnTriggerEnter(Collider other) { 
+        if (other.gameObject.CompareTag("Moneda")) { 
+            other.gameObject.SetActive(false); //Aqui desaparece moneda
+            Debug.Log("¡Moneda recogida!"); 
+        } 
+    } 
+} 
